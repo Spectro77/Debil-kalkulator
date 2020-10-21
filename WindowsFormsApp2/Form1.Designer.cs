58e9f5c24d73936e4155310186fe0de567375a82
Spectro77 <46094261+Spectro77@users.noreply.github.com>
2020-10-21 19:09:13 +0200

    upload

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "C";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(16, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 108);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "OBLICZ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(96, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(33, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(96, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(16, 209);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Funkcja";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.Blue;
+            series2.LabelBorderWidth = 5;
+            series2.Legend = "Legend1";
+            series2.Name = "Miejsca zerowe";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Color = System.Drawing.Color.Lime;
+            series3.Legend = "Legend1";
+            series3.Name = "Wierzchołek";
+            series3.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(713, 376);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click_1);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Kierunek ramion",
+            "Punkt przecięcia osi oY",
+            "Iloczyn pierwiastków",
+            "Suma pierwiastków",
+            "Suma odwrotności pierwiastków",
+            "Suma odwrotności kwadratów pierwiastków"});
+            this.checkedListBox1.Location = new System.Drawing.Point(135, 10);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(594, 94);
+            this.checkedListBox1.TabIndex = 24;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(135, 110);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(594, 93);
+            this.richTextBox1.TabIndex = 25;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(741, 597);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Debil Kalkulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text =="" || textBox2.Text =="" || textBox3.Text == "")
+            {
+                MessageBox.Show("Wszystkie pola muszą być uzupełnione");
+            }
+            else
+            {
+                try
+                {
+                    double A = Convert.ToDouble(textBox1.Text);
+                    double B = Convert.ToDouble(textBox2.Text);
+                    double C = Convert.ToDouble(textBox3.Text);
+                    if (A == 0)
+                    {
+                        MessageBox.Show("Wartość A nie może być równa 0");
+                    }
+                    else
+                    {
+                        richTextBox1.Text = "";
+                        double DELTA = (B * B) - (4 * A * C);
+                        double P = Math.Round((-B) / (2 * A),2);
+                        double Q = Math.Round((-DELTA) / (4 * A),2);
+                        double X1 = Math.Round(((-B) - Math.Sqrt(DELTA)) / (2 * A), 2);
+                        double X2 = Math.Round(((-B) + Math.Sqrt(DELTA)) / (2 * A), 2);
+                        double przedl_X1 = (((X1 - (P - X1)) * (X1 - (P - X1))) * A) + ((X1 - (P - X1)) * B) + C;
+                        double przedl_X2 = (((X2 + (X2 - P)) * (X2 + (X2 - P))) * A) + ((X2 + (X2 - P)) * B) + C;
+                        double iloczyn_pierw = C / A;
+                        double suma_pierw = (-B) / A;
+                        double suma_odwrot_pierw = (-B) / C;
+                        double suma_odwrot_kwadrat_pierw = (((B * B) / A) - (2 * C)) / ((C * C) / A);
+                        if (DELTA > 0)
+                        {
+                            richTextBox1.AppendText($"Delta = {DELTA}\n");
+                            richTextBox1.AppendText($"W = [{P}, {Q}]\n");
+                            richTextBox1.AppendText( $"X₁ = {X1}, X₂ = {X2}\n");
+                            foreach (string opcje in checkedListBox1.CheckedItems)
+                            {
+                                switch (opcje)
+                                {
+                                    case "Kierunek ramion":
+                                        if(A > 0)
+                                        {
+                                            richTextBox1.AppendText($"Ramiona są skierowane w górę\n");
+                                        }
+                                        if (A < 0)
+                                        {
+                                            richTextBox1.AppendText($"Ramiona są skierowane w dół\n");
+                                        }
+                                        break;
+                                    case "Punkt przecięcia osi oY":
+                                        richTextBox1.AppendText($"Punkt przecięcia osi oY: {C}\n");
+                                        break;
+                                    case "Iloczyn pierwiastków":
+                                        richTextBox1.AppendText($"Iloczyn pierwiastków: {iloczyn_pierw}\n");
+                                        break;
+                                    case "Suma pierwiastków":
+                                        richTextBox1.AppendText($"Suma pierwiastków: {suma_pierw}\n");
+                                        break;
+                                    case "Suma odwrotności pierwiastków":
+                                        richTextBox1.AppendText($"Suma odwrotności pierwiastków: {suma_odwrot_pierw}\n");
+                                        break;
+                                    case "Suma odwrotności kwadratów pierwiastków":
+                                        richTextBox1.AppendText($"Suma odwrotności kwadratów pierwiastków: {suma_odwrot_kwadrat_pierw}\n");
+                                        break;
+                                }
+                            }
+                            this.chart1.Series["Miejsca zerowe"].Points.Clear();
+                            this.chart1.Series["Miejsca zerowe"].Points.AddXY(X1, 0);
+                            this.chart1.Series["Miejsca zerowe"].Points.AddXY(X2, 0);
+                            this.chart1.Series["Funkcja"].Points.Clear();
+                            this.chart1.Series["Funkcja"].Points.AddXY(X1-(P-X1), przedl_X1);
+                            this.chart1.Series["Funkcja"].Points.AddXY(X1, 0);
+                            this.chart1.Series["Funkcja"].Points.AddXY(P, Q);
+                            this.chart1.Series["Funkcja"].Points.AddXY(X2, 0);
+                            this.chart1.Series["Funkcja"].Points.AddXY(X2+(X2-P), przedl_X1);
+                            this.chart1.Series["Wierzchołek"].Points.Clear();
+                            this.chart1.Series["Wierzchołek"].Points.AddXY(P, Q);
+
+
+                        }
+                        if(DELTA == 0)
+                        {
+                            richTextBox1.AppendText($"Delta = {DELTA}\n");
+                            richTextBox1.AppendText($"W = [{P}, {Q}]\n");
+                            richTextBox1.AppendText($"X₀ = {X1}\n");
+                            foreach (string opcje in checkedListBox1.CheckedItems)
+                            {
+                                switch (opcje)
+                                {
+                                    case "Kierunek ramion":
+                                        if (A > 0)
+                                        {
+                                            richTextBox1.AppendText($"Ramiona są skierowane w górę\n");
+                                        }
+                                        if (A < 0)
+                                        {
+                                            richTextBox1.AppendText($"Ramiona są skierowane w dół\n");
+                                        }
+                                        break;
+                                    case "Punkt przecięcia osi oY":
+                                        richTextBox1.AppendText($"Punkt przecięcia osi oY: {C}\n");
+                                        break;
+                                }
+                            }
+                            this.chart1.Series["Funkcja"].Points.Clear();
+                            this.chart1.Series["Miejsca zerowe"].Points.Clear();
+                            this.chart1.Series["Wierzchołek"].Points.Clear();
+                            this.chart1.Series["Funkcja"].Points.AddXY(X1, 0);
+                            this.chart1.Series["Funkcja"].Points.AddXY(P, Q);
+                        }
+                        if (DELTA < 0)
+                        {
+                            richTextBox1.AppendText($"Delta = {DELTA}\n");
+                            richTextBox1.AppendText($"W = [{P}, {Q}]\n");
+                            richTextBox1.AppendText("Funkcja nie ma miejsc zerowych\n");
+                            foreach (string opcje in checkedListBox1.CheckedItems)
+                            {
+                                switch (opcje)
+                                {
+                                    case "Kierunek ramion":
+                                        if (A > 0)
+                                        {
+                                            richTextBox1.AppendText($"Ramiona są skierowane w górę\n");
+                                        }
+                                        if (A < 0)
+                                        {
+                                            richTextBox1.AppendText($"Ramiona są skierowane w dół\n");
+                                        }
+                                        break;
+                                    case "Punkt przecięcia osi oY":
+                                        richTextBox1.AppendText($"Punkt przecięcia osi oY: {C}\n");
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Wartość jest zbyt duża lub nieprawidłowa");
+                }
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
